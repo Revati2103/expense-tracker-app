7a451823c0623ec14b7e86750502b0986b3afdca
Revati <revati.v.bordawekar@gmail.com>
2023-02-22 13:30:33 -0800

    auth routes protected

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import { useAuthContext } from "./hooks/useAuthContext";
 import Navbar from "./components/Navbar/Navbar";
 import Home from './pages/home/Home'
@@ -8,15 +8,18 @@ import Signup from './pages/signup/Signup'
 
 
 function App() {
-  const {authIsReady} = useAuthContext();
+  const {authIsReady, user} = useAuthContext();
   return (
     <div className="App">
  {  authIsReady &&   (<BrowserRouter>
       <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
+
+        <Route exact path = '/' 
+        element={user ? <Home /> : <Navigate to="/login" />}
+        />
+          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+          <Route path="signup" element={!user ? <Signup /> : <Navigate to="/" />} />
        </Routes>
       </BrowserRouter>
 )}
