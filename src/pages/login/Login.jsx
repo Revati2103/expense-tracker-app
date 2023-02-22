3cb2347b40ae34555e8b6312ddf4580870c4384c
Revati <revati.v.bordawekar@gmail.com>
2023-02-22 10:05:04 -0800

    login hook added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import styles from './Login.module.css'
+import {useLogin} from '../../hooks/useLogin'
 
 const Login = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const {login, error, isPending} = useLogin();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password)
+  login(email, password)
   }
 
   return (
@@ -34,7 +37,9 @@ const Login = () => {
             />
         </label>
 
-      <button className="btn">Login</button>
+ {  !isPending && <button className="btn">Login</button>}
+      {isPending && <button className='btn'>Loading</button>}
+      {error && <p>{error}</p>}
       </form>
   )
 }
